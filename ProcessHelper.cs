d5d8bba73ce4b47f7c9b3e66800665c52c7e1236
BinGuoGuo <30474925+BinGuoGuo@users.noreply.github.com>
2020-12-29 15:21:39 +0800

    直接提交代码

---
+++ ProcessHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NuGetPusher
+{
+    public static class ProcessHelper
+    {
+        public static Process Run(string exe, string arguments, string workingDirectory = ".", bool waitExit = false)
+        {
+            try
+            {
+                bool redirectStandardOutput = true;
+                bool redirectStandardError = true;
+                bool useShellExecute = false;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    redirectStandardOutput = false;
+                    redirectStandardError = false;
+                    useShellExecute = true;
+                }
+
+                if (waitExit)
+                {
+                    redirectStandardOutput = true;
+                    redirectStandardError = true;
+                    useShellExecute = false;
+                }
+
+                ProcessStartInfo info = new ProcessStartInfo
+                {
+                    FileName = exe,
+                    Arguments = arguments,
+                    CreateNoWindow = true,
+                    UseShellExecute = useShellExecute,
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = redirectStandardOutput,
+                    RedirectStandardError = redirectStandardError,
+                };
+
+                using Process process = Process.Start(info);
+
+                if (waitExit)
+                {
+                    process.OutputDataReceived += (sender, e) =>
+                     {
+                         if(null != e)
+                         {
+                             Console.WriteLine(e.Data);
+                         }
+                     };
+                    process.BeginOutputReadLine();
+                    process.WaitForExit();
+                    if (process.ExitCode != 0)
+                    {
+                        throw new Exception($"{process.StandardOutput.ReadToEnd()} {process.StandardError.ReadToEnd()}");
+                    }
+                }
+
+                return process;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"dir: {Path.GetFullPath(workingDirectory)}, command: {exe} {arguments}", e);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NuGetPusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arg[0] $(ProjectDir)/bin/Debug 发布文件所在目录
+            // arg[1] $(TargetPath)           项目程序集路径
+            // arg[2] ApiKey                  仓库密钥
+            // arg[3] Url                     仓库地址
+            try
+            {
+                
+                if (args.Length != 4)
+                    throw new Exception($"invalid parameters: {System.Text.Json.JsonSerializer.Serialize(args)}");
+                string publishDir = args[0];
+                string assemblyFile = args[1];
+                string apiKey = args[2];
+                string url = args[3];
+
+                var assemblyName = System.Reflection.AssemblyName.GetAssemblyName(assemblyFile);
+                var nupkgName = $"{assemblyName.Name}.{assemblyName.Version.ToString(3)}.nupkg";
+                var fullName = System.IO.Path.Combine(publishDir, nupkgName);
+                Console.WriteLine($"nuget push {fullName} {apiKey} -Source {url}");
+                ProcessHelper.Run("nuget.exe", $"push {fullName} {apiKey} -Source {url}", waitExit: true);
+                Console.WriteLine("publish succeed!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
